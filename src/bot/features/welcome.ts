f7b7501630f851df17f5e54f7fb144fbdb4f30c4
aylivdele <irassomahin@gmail.com>
2025-12-30 17:24:55 +0500

    fixes

--- src/bot/features/welcome.ts
+++ src/bot/features/welcome.ts
@@ -9,12 +9,12 @@ const composer = new Composer<Context>()
 
 const feature = composer.chatType('private')
 
-feature.command(startMenuCallbackData, logHandle('command-start'), (ctx) => {
+feature.command(startMenuCallbackData, logHandle('command-start'), async (ctx) => {
   if (!ctx.session.userInfo) {
     ctx.notifyAdmin(`Новый пользователь в боте: @${ctx.from.username}`)
   }
-  return insertNewUser(ctx.from, ctx.db)
-    .then(() => ctx.answerWithMedia(startMenuCallbackData, `Отлично, давай определимся, что тебе сейчас ближе 👇
+  await insertNewUser(ctx.from, ctx.db)
+  return await ctx.answerWithMedia(startMenuCallbackData, `Отлично, давай определимся, что тебе сейчас ближе 👇
 
 — Крипто-школа - для базового понимания криптовалют 
 — Трейдинг-курс - самостоятельно торгуй и зарабатывай уже через месяц
@@ -22,12 +22,13 @@ feature.command(startMenuCallbackData, logHandle('command-start'), (ctx) => {
 — Торговые роботы - автоматизиуй трейдинг, ты спишь, а твои деньги работают 10% в месяц
 — Партнерская программа - зарабатывай 11.000RUB за каждого приглашенного друга
 
-Если хочешь связаться со мной лично и сразу обсудить детали - нажимай «Хочу пообщаться лично»`, { keyboard: createStartKeyboard(ctx) }))
+Если хочешь связаться со мной лично и сразу обсудить детали - нажимай «Хочу пообщаться лично»`, { keyboard: createStartKeyboard(ctx) })
 })
 
 feature
   .callbackQuery(startMenuCallbackData, async (ctx) => {
     await ctx.answerCallbackQuery()
+    ctx.updateUserState(startMenuCallbackData)
     return ctx.answerWithMedia(startMenuCallbackData, `Отлично, давай определимся, что тебе сейчас ближе 👇
 
 — Крипто-школа - для базового понимания криптовалют 
