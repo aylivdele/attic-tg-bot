16e06a7989028dd8c686d644b3efb37103e3e462
aylivdele <irassomahin@gmail.com>
2026-01-05 16:14:35 +0500

    fix notification

--- src/bot/features/update-notification.ts
+++ src/bot/features/update-notification.ts
@@ -1,14 +1,17 @@
 import type { Context } from '#root/bot/context.js'
+import { logger } from '#root/logger.js'
 import { Composer } from 'grammy'
 
 const composer = new Composer<Context>()
 
 const feature = composer.chatType('private')
 
-feature.use((ctx) => {
+feature.use(async (ctx, next) => {
   if (ctx.session.userInfo && (ctx.session.userInfo.last_update + 1000 * 60 * 60 * 24) < Date.now()) {
     ctx.notifyAdmin(`Пользователь восстановил активность в боте: @${ctx.session.userInfo.username}`)
   }
+  logger.debug(`Last update of ${ctx.from.username}(${ctx.from.id}) = ${ctx.session.userInfo ?? 'not found'}`)
+  await next()
 })
 
 export { composer as cameBackFeature }
